ed4c46ad623a515c869fe6ee3c880a00335627bd
JonathanACS <jonathan.beze580@gmail.com>
2024-06-14 11:31:30 +0200

    ajout du fait de supprimer un jeu

--- src/backoffice/backend_game_delete.php
+++ src/backoffice/backend_game_delete.php
@@ -21,6 +21,19 @@ if (isset($_GET["id"]) && !empty($_GET["id"])) {
         exit();
     }
 
+    // Sélection du jeu à supprimer
+    $sql_select = "SELECT * FROM `plateforme` WHERE `id_game` = :id_game;";
+    $query_select = $db->prepare($sql_select);
+    $query_select->bindValue(':id_game', $id_game, PDO::PARAM_INT);
+    $query_select->execute();
+    $result_plateforme = $query_select->fetch();
+
+    if (!$result_plateforme) {
+        $_SESSION["erreur"] = "L'ID en question n'existe pas.";
+        header("Location: backend_game_list.php");
+        exit();
+    }
+
     // Récupération des chemins des images à supprimer
     $picture_right = $result['picture_right'];
     $picture_left = $result['picture_left'];
@@ -34,6 +47,12 @@ if (isset($_GET["id"]) && !empty($_GET["id"])) {
 
     // Suppression de l'entrée dans la base de données
     $sql_delete = "DELETE FROM `jeux` WHERE `id_game` = :id_game;";
+    $query_delete = $db->prepare($sql_delete);
+    $query_delete->bindValue(':id_game', $id_game, PDO::PARAM_INT);
+    $query_delete->execute();
+
+    // Suppression de l'entrée dans la base de données
+    $sql_delete = "DELETE FROM `plateforme` WHERE `id_game` = :id_game;";
     $query_delete = $db->prepare($sql_delete);
     $query_delete->bindValue(':id_game', $id_game, PDO::PARAM_INT);
     $query_delete->execute();
